36a64ebf8b6cfe97f52356c90571280ffa296c67
Basil <vasyaabr@gmail.com>
2012-04-12 13:17:49 +0000

    Added email and active status to Account class and table

--- db/migrate/20120412111351_create_accounts.rb
+++ db/migrate/20120412111351_create_accounts.rb
@@ -1,6 +1,6 @@
 class CreateAccounts < ActiveRecord::Migration
   MORE_ACCOUNTS = [
-    {:name => 'Basil', :account => '9211147767', :pass => '666', :sum => 0, :credit => 1},
+    {:name => 'Basil', :account => '9211147767', :pass => '1234', :sum => 500, :credit => 1, :mail => 'vasyaabr@gmail.com', :active => 1},
   ]
   def up
     create_table :accounts do |t|
@@ -9,9 +9,12 @@ class CreateAccounts < ActiveRecord::Migration
       t.string :pass
       t.integer :sum
       t.integer :credit
+      t.string :mail
+      t.integer :active
     end
-#    MORE_ACCOUNTS.each do |acc|
-#      Account.create!(acc)
+    MORE_ACCOUNTS.each do |acc|
+      Account.create!(acc)
+    end
   end
 
   def down
